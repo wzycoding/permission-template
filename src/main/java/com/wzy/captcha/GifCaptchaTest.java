eebfc70098bfcdce7106d5fe61939448131133a0
wzy <842089160@qq.com>
2020-04-16 20:10:46 +0800

    修改gif验证码测试类

--- src/main/java/com/wzy/captcha/GifCaptchaTest.java
+++ src/main/java/com/wzy/captcha/GifCaptchaTest.java
@@ -7,7 +7,9 @@ public class GifCaptchaTest {
     public static void main(String[] args) throws FileNotFoundException {
         Captcha captcha = new SpecCaptcha(150,40,5);// png格式验证码
         captcha.out(new FileOutputStream("/Users/wzy/1.png"));
+        System.out.println(captcha.text());
         captcha = new GifCaptcha(150,40,5);//   gif格式动画验证码
         captcha.out(new FileOutputStream("/Users/wzy/1.gif"));
+        System.out.println(captcha.text());
     }
 }
